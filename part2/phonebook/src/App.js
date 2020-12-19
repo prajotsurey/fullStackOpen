55992f94b740ca8baa76b983ba0f1d012354cd09
prajotsurey <prajotsurey@gmail.com>
2020-12-19 13:30:06 +0530

    2.11 phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Search from './components/Search'
 import Form from './components/Form'
 import Display from './components/Display'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('Enter name')
   const [ newNumber, setNewNumber ] = useState('Enter number')
   const [ filter, setFilter ] = useState('')
 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
   const personsToShow = persons.filter(person=>person.name.toUpperCase().search(filter.toUpperCase()) !== -1)
   
   const handleFilterChange= (event) => {
